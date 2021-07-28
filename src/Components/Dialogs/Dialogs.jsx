63e3d89e244d7b19f4e44ea1accfd109f3c94ec1
LGers <lgers@mail.ru>
2021-07-29 01:41:09 +0300

    Lesson 43 CLEAR- v2

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,32 +2,47 @@ import React from "react";
 import st from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {
-    addMsgAC,
-    onMsgChangeAC,
-} from "../../Redux/store";
+
+import {addPostAC, updNewPostTextAC} from "../../Redux/profile-reducer";
+import {addMessageAC, updNewMessageBodyAC} from "../../Redux/dialogs-reducer";
 
 
 const Dialogs = (props) => {
 
-    let state=props.store.getState().dialogPage
+    //let state=props.store.getState().dialogPage
 
-    let dialogsEl = state.dataDialogs.map(d => <DialogItem idItem={d.id} name={d.name}/>)
-    let messageEl = state.dataMsgs.map(m => <Message messageId={m.id} message={m.message}/>)
-    let newMessageBody = state.newMsgBody;
-
+    let dialogsEl = props.dataDialogs.map(d => <DialogItem idItem={d.id} name={d.name}/>)
+    let messageEl = props.dataMsgs.map(m => <Message messageId={m.id} message={m.message}/>)
+    let newMessageBody = props.newMsgBody;
+    //
     let newMsgElement = React.createRef();
+    //
+    // let addMsg = () => {
+    //
+    //     props.dispatch(addMsgAC())
+    // }
+    //
+    // // let onMsgChange = (e) => {
 
-    let addMsg = () => {
+//-----------------------------------------
+    //let state = props.store.getState();
 
-        props.dispatch(addMsgAC())
+    let onAddMsg = () => {
+        props.addMsg();
+        //props.store.dispatch(addMessageAC())
+        // store.dispatch(addPostAC())
     }
 
-    let onMsgChange = (e) => {
-        let body = e.target.value;
-        props.dispatch(onMsgChangeAC(body));
+    let onMsgChange = () => {
+        //let action = updNewMessageBodyAC(e);
+        //props.store.dispatch(action)
 
+        //let body = e.target.value;
+        let body = newMsgElement.current.value;
+        props.onMsgChange(body);
     }
+//-----------------------------------------
+
 
     return (
         <div className={st.dialogs}>
@@ -41,7 +56,7 @@ const Dialogs = (props) => {
                               value={newMessageBody}/>
                 </div>
                 <div>
-                    <button onClick={addMsg}>Add msg</button>
+                    <button onClick={onAddMsg}>Add msg</button>
                 </div>
             </div>
         </div>
--- src/Components/Dialogs/DialogsContainer.jsx
+++ src/Components/Dialogs/DialogsContainer.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import st from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {addMessageAC, updNewMessageBodyAC} from "../../Redux/dialogs-reducer";
-/* import {
-     addMsgAC,
-     onMsgChangeAC,
- } from "../../Redux/store";
-*/
-const Dialogs = (props) => {
-//debugger
-    let state=props.store.getState().dialogPage
+import Dialogs from "./Dialogs";
+import {addPostAC, updNewPostTextAC} from "../../Redux/profile-reducer";
 
-    let dialogsEl = state.dataDialogs.map(d => <DialogItem idItem={d.id} name={d.name}/>)
+const DialogsContainer = (props) => {
+    let state = props.store.getState().dialogPage
+
+    /*let dialogsEl = state.dataDialogs.map(d => <DialogItem idItem={d.id} name={d.name}/>)
     let messageEl = state.dataMsgs.map(m => <Message messageId={m.id} message={m.message}/>)
     let newMessageBody = state.newMsgBody;
 
@@ -26,26 +22,46 @@ const Dialogs = (props) => {
         let body = e.target.value;
         props.store.dispatch(updNewMessageBodyAC(body));
 
+    }*/
+
+    //let state = props.store.getState();
+
+    let addMsg = () => {
+        props.store.dispatch(addMessageAC())
+    }
+
+    let onMsgChange = (e) => {
+        let action = updNewMessageBodyAC(e);
+        props.store.dispatch(action)
     }
 
     return (
-        <div className={st.dialogs}>
-            <div className={st.dialogsItems}>
-                {dialogsEl}
-            </div>
-            <div className={st.messages}>
-                {messageEl}
-                <div>
-                    <textarea placeholder={'Enter your message'} onChange={onMsgChange} ref={newMsgElement}
-                              value={newMessageBody}/>
-                </div>
-                <div>
-                    <button onClick={addMsg}>Add msg</button>
-                </div>
+        <Dialogs
+            addMsg={addMsg}
+            onMsgChange={onMsgChange}
 
-            </div>
+            dataDialogs={state.dataDialogs}
+            dataMsgs={state.dataMsgs}
+            newMsgBody={state.newMsgBody}
+        />)
+    /*(<div className={st.dialogs}>
+        <div className={st.dialogsItems}>
+            {dialogsEl}
         </div>
-    )
+        <div className={st.messages}>
+            {messageEl}
+            <div>
+                <textarea placeholder={'Enter your message'} onChange={onMsgChange} ref={newMsgElement}
+                          value={newMessageBody}/>
+            </div>
+            <div>
+                <button onClick={addMsg}>Add msg</button>
+            </div>
+
+        </div>
+    </div>)
+    */
+
 }
 
-export default Dialogs;
+export default DialogsContainer;
