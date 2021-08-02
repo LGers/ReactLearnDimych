98557f65402c3902c4aff2ad443915d657cd9a84
LGers <lgers@mail.ru>
2021-08-02 12:59:30 +0300

    Lesson 53

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,32 +4,22 @@ import st from './Users.module.css';
 import * as axios from "axios";
 import userPhotoDefault from '../../assets/img/default-user-icon-15.jpg'
 
+class Users extends React.Component {
 
-let Users = (props) => {
+    constructor(props) {
+        super(props);
+        alert('new')
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        });
 
-    let getUsers = () => {
-        if (props.users.length===0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
-            });
-        }
     }
 
-
-    let usersEl = props.users.map(u => <UserItem
-        idItem={u.id}
-        name={u.name}
-        key={u.id}
-        status={u.status}
-        photos={u.photos}
-        followed={u.followed}
-    />)
-
-    return (
-        <div>
-            <button onClick={getUsers}>GetUsers</button>
-            {
-                props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img className={st.avaPhoto} src={
@@ -40,16 +30,16 @@ let Users = (props) => {
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    props.unFollowClick(u.id)
+                                    this.props.unFollowClick(u.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    props.followClick(u.id)
+                                    this.props.followClick(u.id)
                                 }}>Follow</button>
                             }
 
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -61,13 +51,13 @@ let Users = (props) => {
                             <div>{'City'}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
+                    </div>)
+                }
+            </div>
+        )
 
-
-    )
-
+    }
 
 }
+
 export default Users;
